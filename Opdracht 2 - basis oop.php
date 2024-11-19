f5c3abc7dbc2ea4f3825da001bb9b79f27914621
author name <test@test.test>
2024-11-19 11:22:50 +0100

    Extra tests opdr 2

--- Opdracht 2 - basis oop.php
+++ Opdracht 2 - basis oop.php
@@ -35,14 +35,18 @@
 //controle code
 
 $turtle = new Turtle("Leonardo", 5);
+$turtle2 = new Turtle("Donatello", 9);
 echo "Succes: Turtle made succesfully<br>";
-if ($turtle->displayInfo() == "Leonardo, 5, 0"){
+if ($turtle->displayInfo() == "Leonardo, 5, 0" && $turtle2->displayInfo() == "Donatello, 9, 0"){
     echo "Succes: Turtle values set correctly by constructor<br>";
 }
 else{
     echo "Mistake: Turtle values not set correctly by constructor.<br>
-Should be: Leonardo, 5, 0<br>
+First turtle should be: Leonardo, 5, 0<br>
+Instead got: " . $turtle->displayInfo() . "<br>" . "
+    Second turtle should be: Donatello, 9, 0<br>
 Instead got: " . $turtle->displayInfo() . "<br>";
+    echo "Remember to not manually set the info in the constructor, but to use function arguments instead.";
     exit(1);
 }
 
